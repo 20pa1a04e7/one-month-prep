6ce799cbc9aa874879af49248376332b3d764a82
20pa1a04e7 <123354325+20pa1a04e7@users.noreply.github.com>
2023-02-10 16:14:04 +0530

    Create Merge two sorted linked lists.java

---
+++ 10-02-2023/Merge two sorted linked lists.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the mergeLists function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+          SinglyLinkedListNode dummy = new SinglyLinkedListNode(0);
+        SinglyLinkedListNode head = dummy;
+           while(head1!=null && head2!=null){
+               if(head1.data < head2.data){
+                   dummy.next = head1;
+                   head1 = head1.next;
+               }
+               else {
+                   dummy.next = head2;
+                   head2 = head2.next;                
+               }
+                dummy = dummy.next;
+           }
+           while(head1!=null){
+               dummy.next = head1;
+               head1= head1.next;
+                dummy = dummy.next;
+           }
+           while(head2!=null){
+               dummy.next = head2;
+               head2= head2.next;
+                dummy = dummy.next;
+           }
+           return head.next;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
